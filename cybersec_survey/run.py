4b3440947baf0563aa3b5cebac169397324e7e02
llinauer <l.linauer@gmx.at>
2025-04-30 08:03:04 +0200

    Add debug statements

--- cybersec_survey/run.py
+++ cybersec_survey/run.py
@@ -2,9 +2,12 @@
 
 from cybersec_survey.app import app
 from cybersec_survey.config import Config
+from werkzeug.debug import DebuggedApplication
 
 
 def main():
+    if Config.DEBUG:
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
     app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
 
 
